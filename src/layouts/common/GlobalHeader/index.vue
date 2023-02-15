4876c5d60cc5fb026e579a3aa7ddba0719e11c05
MiaoXu <1119610560@qq.com>
2023-02-15 17:24:03 +0800

    feat:首页

--- src/layouts/common/GlobalHeader/index.vue
+++ src/layouts/common/GlobalHeader/index.vue
@@ -11,7 +11,7 @@
             <theme-mode />
             <system-message />
             <setting-button v-if="showButton" />
-            <user-avatar />
+            <user-avatar  />
         </div>
     </dark-mode-container>
 </template>
@@ -55,4 +55,6 @@ const showButton = PROD && VITE_VERCEL !== 'Y'
 .global-header {
     box-shadow: 0 1px 2px rgb(0 21 41 / 8%);
 }
+
+
 </style>
