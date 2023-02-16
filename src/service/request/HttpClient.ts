3a02b8a7fcad0207aaa739fd4bcf251790e10ea5
DNN <1524757775@qq.com>
2023-02-16 15:26:45 +0800

    通用错误处理, 增加消息提示

--- src/service/request/HttpClient.ts
+++ src/service/request/HttpClient.ts
@@ -134,25 +134,42 @@ export interface HttpResponse<R = any> {
     msg: string
 }
 
+/**
+ * 服务器通用响应结构
+ */
+export class GenericResponseError extends Error {
+    public readonly res: HttpResponse
+
+    public constructor(message: string, res: HttpResponse) {
+        super(message)
+        this.res = res
+    }
+}
+
 function HttpResponseAssertion(data: any): Promise<any> {
     if (typeof data.code === 'number') {
         if (data.code === 0) {
             return Promise.resolve(data)
         } else {
-            return Promise.reject(data)
+            return Promise.reject(new GenericResponseError('通用响应结构错误', data))
         }
     }
-    return Promise.reject(data)
+    return Promise.resolve(data)
 }
 
 /**
  * 通用错误处理
+ * @param error 捕获错误
+ * @param showMessage 如果错误类型是 {@link GenericResponseError} 是否展示错误消息. 默认是;
+ * 当手动处理异常时可以指定 false, 避免重复弹窗
  */
-export function StandardErrorProcessor(error: any): void {
+export function StandardErrorProcessor(error: any, showMessage: boolean = true): void {
     console.debug('[HTTP 响应错误]: ', error)
 
     if (error instanceof AxiosError) {
         window.$message?.error(error.message)
     }
-
+    if (showMessage && error instanceof GenericResponseError) {
+        window.$message?.error(error.res?.msg)
+    }
 }
